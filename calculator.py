f58669f4fb5c55975b8c065c0177c2d99d6b4baa
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 16:21:48 -0700

    basic program running w/ main funtctionality.

--- calculator.py
+++ calculator.py
@@ -7,21 +7,41 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
-
-# Your code goes here
 #will hold requested users calculation to perform
-calculation = raw_input("> ")
-#print calculation
+calculation = None
 
-#......
-token = calculation.split(" ")
-print token
+while calculation != "q":
+	calculation = raw_input("> ")
+	#print calculation
 
-#determine what mathematical equation to perform based on users first inputted value
-if token[0] == "+":
-	print add(int(token[1]), int(token[2]))
+	#......
+	token = calculation.split(" ")
+	#print token
 
-elif token[0] == "-":
-	print subtract(int(token[1]), int(token[2]))
-else:
-	print "what?"
+	#determine what mathematical equation to perform based on users first inputted value
+	if token[0] == "+":
+		print add(int(token[1]), int(token[2]))
+
+	elif token[0] == "-":
+		print subtract(int(token[1]), int(token[2]))
+
+	elif token[0] == "*":
+		print multiply(int(token[1]), int(token[2]))
+	
+	elif token[0] == "/":
+		print divide(int(token[1]), int(token[2]))
+
+	elif token[0] == "square":
+		print square(int(token[1]))
+	
+	elif token[0] == "cube":
+		print cube(int(token[1]))
+
+	elif token[0] == "pow":
+		print power(int(token[1]), int(token[2]))
+
+	elif token[0] == "mod":
+		print mod(int(token[1]), int(token[2]))	
+
+	else:
+		print "what?"
